f98c9eeb4aea53a97078236a5a0129e9fde17440
semyon <semyon.mihalich@gmail.com>
2023-07-08 10:42:46 +0500

    Full version

--- src/main/java/uz/shabbat/parsing/ParsingChabadOrg.java
+++ src/main/java/uz/shabbat/parsing/ParsingChabadOrg.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import uz.shabbat.Shabbat;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 /**
  @author Jebrak Semyon
--- src/main/java/uz/shabbat/parsing/ParsingChapterChabadOrg.java
+++ src/main/java/uz/shabbat/parsing/ParsingChapterChabadOrg.java
@@ -6,9 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.io.File;
-import java.io.IOException;
+
 /**
  @author Jebrak Semyon
  @version 1.0
